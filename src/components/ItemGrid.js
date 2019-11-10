2efa5f64de11aa3f0f842c38443a688010f9ff07
Adam Hussain <adamh93@verizon.net>
2019-11-10 18:11:20 -0500

    done: Added price menu, changed item-area height to 100vh; todo: Remove background color on side menu and item grid, Add pictures to item boxes on the grid

--- src/components/ItemGrid.js
+++ src/components/ItemGrid.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Slider from 'react-rangeslider'
 import '../styles.css';
+import 'react-rangeslider/lib/index.css'
 
 
 class ItemGrid extends Component {
-  constructor(props){
-    super(props);
+  constructor(props, context){
+    super(props, context);
 
-    this.state = {};
+    this.state = {
+      slidervalue: 5
+    };
 
     this.openSearchBar = this.openSearchBar.bind(this);
   }
@@ -16,7 +20,22 @@ class ItemGrid extends Component {
     this.setState({showSearchBar: !this.state.showSearchBar});
   }
 
+  handleChangeStart = () => {
+    console.log('Change event started')
+  };
+
+  handleChange = value => {
+    this.setState({
+      value: value
+    })
+  };
+
+  handleChangeComplete = () => {
+    console.log('Change event completed')
+  };
+
   render(){
+    const { slidervalue } = this.state
     return(
       <div id="item-area">
         <div id="side-menu">
@@ -76,36 +95,55 @@ class ItemGrid extends Component {
           </div>
           <div className="color-box">
             <h5>Color:</h5>
-              <ul className="color-list">
-                <li className="swatch color-swatch-black">
+            <ul className="color-list">
+              <li className="swatch color-swatch-black">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-blue">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-red">
+                <a>  </a>
+              </li>   <br />
+              <li className="swatch color-swatch-nude">
                   <a>  </a>
-                </li>
-                <li className="swatch color-swatch-blue">
-                  <a>  </a>
-                </li>
-                <li className="swatch color-swatch-red">
-                  <a>  </a>
-                </li>   <br />
-                <li className="swatch color-swatch-green">
-                    <a>  </a>
-                </li>
-                <li className="swatch color-swatch-white">
-                  <a>  </a>
-                </li>
-                <li className="swatch color-swatch-grey">
-                  <a>  </a>
-                </li>   <br />
-                <li className="swatch color-swatch-purple">
-                  <a>  </a>
-                </li>
-                <li className="swatch color-swatch-brown">
-                  <a>  </a>
-                </li>
-                <li className="swatch color-swatch-yellow">
-                  <a>  </a>
-                </li>  <br />
-
-              </ul>
+              </li>
+              <li className="swatch color-swatch-white">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-grey">
+                <a>  </a>
+              </li>   <br />
+              <li className="swatch color-swatch-purple">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-brown">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-yellow">
+                <a>  </a>
+              </li>  <br />
+              <li className="swatch color-swatch-orange">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-pink">
+                <a>  </a>
+              </li>
+              <li className="swatch color-swatch-green">
+                <a>  </a>
+              </li>  <br />
+            </ul>
+          </div>
+          <div className="price-range">
+            <h5 className="">Price:</h5>
+            <ul className="prices">
+              <li><a className="pointer">$10</a></li>
+              <li><a className="pointer">$20</a></li>
+              <li><a className="pointer">$30</a></li>
+              <li><a className="pointer">$40</a></li>
+              <li><a className="pointer">$50</a></li>
+              <li><a className="pointer">$100</a></li>
+            </ul>
           </div>
         </div>
         <div id="item-grid">
