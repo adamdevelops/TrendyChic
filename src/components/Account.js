9df293ffc4e2c4ae9e86a7d21eb959ff0402f034
Adam Hussain <adamh93@verizon.net>
2019-11-14 23:31:10 -0500

    done: Create Recent Orders div that uses markup of checkout items for each order item, Create Favorites div floated to right with smaller blocks separated by hrs with each item; todo: Add state to pages that will track user activity, Work connecting Redux to app

--- src/components/Account.js
+++ src/components/Account.js
@@ -19,9 +19,28 @@ class Account extends Component {
       <div id="account-section">
         <div className="recent-orders">
           <h1>Recent Orders</h1>
+            <div className="checkout-item">
+              <img className="checkout-item-pic fleft" src="/tops/7.jpg" />
+              <p className="item-name">White Dress with Black Design</p>
+              <p className="item-price">$15</p>
+              <button className="remove_button">Remove</button>
+            </div>
         </div>
         <div className="favorites">
           <h1>Favorites</h1>
+            <div className="checkout-item">
+              <img className="fav-item-pic fleft" src="/tops/7.jpg" />
+              <p className="item-name">White Dress with Black Design</p>
+              <p className="item-price">$15</p>
+              <button className="remove_button">Remove</button>
+            </div>
+            <hr />
+              <div className="checkout-item">
+                <img className="fav-item-pic fleft" src="/tops/3.jpg" />
+                <p className="item-name">White Dress with Black Design</p>
+                <p className="item-price">$15</p>
+                <button className="remove_button">Remove</button>
+              </div>
         </div>
       </div>
     );
